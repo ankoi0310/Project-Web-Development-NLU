693f749437055da6b02ffddcdb9197cd1d47e3d8
Huỳnh Văn Hữu Ân <19130003@st.hcmuaf.edu.vn>
2021-11-25 02:37:02 +0700

    Login ft. jquery

--- js/login.js
+++ js/login.js
@@ -1,46 +1,46 @@
-let modal = document.getElementById("login");
-let btnLogin = document.getElementById("btnLogin");
-let closeBtn = document.getElementsByClassName("close")[0];
+let modal$ = $("#login");
 
 // Show/hide modal login
-btnLogin.onclick = () => modal.style.display = "block";
-closeBtn.onclick = () => modal.style.display = "none";
-window.onclick = (event) => {
-    if (event.target === modal) modal.style.display = "none";
-}
+$("#btnLogin").on('click', () => modal$.show());
+$(".close").first().on('click', () => modal$.hide());
+$(window).on('click', (e) => {
+    if ($(e.target).is(modal$)) modal$.hide();
+})
 
 // Hover facebook/google button
-let signinFacebook = document.getElementById("signin-facebook");
-let signinGoogle = document.getElementById("signin-google");
-signinFacebook.onpointerenter = () => {
-    if (signinFacebook.style.width !== "395px") {
-        signinGoogle.style.width = "36px";
-        signinGoogle.innerHTML = '<i class="fab fa-google"></i>';
-        signinFacebook.style.width = "395px";
-        setTimeout(() => signinFacebook.innerHTML += ' Sign in with Facebook', 500);
+let signinFacebook$ = $("#signin-facebook");
+let signinGoogle$ = $("#signin-google");
+signinFacebook$.hover(function () {
+    if (signinFacebook$.outerWidth() !== 395) {
+        signinGoogle$.outerWidth(35);
+        signinGoogle$.html('<i class="fab fa-google"></i>');
+        signinFacebook$.outerWidth(395);
+        let html = signinFacebook$.html() + ' Sign in with Facebook';
+        setTimeout(() => signinFacebook$.html(html), 700);
     }
-}
-signinGoogle.onpointerenter = () => {
-    if (signinGoogle.style.width !== "395px") {
-        signinFacebook.style.width = "36px";
-        signinFacebook.innerHTML = '<i class="fab fa-facebook-f"></i>';
-        signinGoogle.style.width = "395px";
-        setTimeout(() => signinGoogle.innerHTML = 'Sign in with Google ' + signinGoogle.innerHTML, 500);
+});
+signinGoogle$.hover(function () {
+    if (signinGoogle$.outerWidth() !== 395) {
+        signinFacebook$.outerWidth(35);
+        signinFacebook$.html('<i class="fab fa-facebook-f"></i>');
+        signinGoogle$.outerWidth(395);
+        let html = 'Sign in with Google ' + signinGoogle$.html();
+        setTimeout(() => signinGoogle$.html(html), 700);
     }
-
-}
+});
 
 // Show/hide password
-let show = document.getElementById("show");
-let inputPassword = document.getElementById("password");
-show.onclick = () => {
-    if (show.classList.contains("fa-eye")) {
-        show.classList.remove("fa-eye");
-        show.classList.add("fa-eye-slash");
-        inputPassword.attributes.getNamedItem("type").value = "password";
+let show$ = $("#show");
+let inputPassword$ = $("#password");
+
+show$.on('click', () => {
+    if (show$.hasClass("fa-eye")) {
+        show$.removeClass("fa-eye");
+        show$.addClass("fa-eye-slash");
+        inputPassword$.attr("type","password");
     } else {
-        show.classList.add("fa-eye");
-        show.classList.remove("fa-eye-slash");
-        inputPassword.attributes.getNamedItem("type").value = "text";
+        show$.addClass("fa-eye");
+        show$.removeClass("fa-eye-slash");
+        inputPassword$.attr("type","text");
     }
-}
+});
